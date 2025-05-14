3ca0dc04ac78236b17b718e265ebddeba027b17f
jaylzd <jaywheels8@gmail.com>
2025-05-14 15:04:09 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+/** 
+ * this class just redirects to Script for the main page
+ * Line 3 2/4 classes
+ * Line 10 Starts code redirection to Script.java
+ */
+import java.util.Scanner;
+public class Main //2/4 classes
+{
+    public static void main(String[] args)
+    {
+        Script.intro();
+        
+    }
+}
