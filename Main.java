cb9a9a1afbf6818bf3fb40fbf923fa4786b065dd
jaylzd <jaywheels8@gmail.com>
2025-05-28 16:15:10 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,23 @@
-/** 
- * this class just redirects to Script for the main page
- * Line 3 2/4 classes
- * Line 10 Starts code redirection to Script.java
- */
 import java.util.Scanner;
-public class Main //2/4 classes
-{
-    public static void main(String[] args)
-    {
+
+/**
+ * The {@code Main} class serves as the entry point of the "Live or Die" game.
+ * It simply redirects to the main menu logic handled in the {@link Script} class.
+ *
+ * <p>Structure Overview:</p>
+ * <ul>
+ *   <li>Line 3: One of the 2/4 core classes</li>
+ *   <li>Line 10: Starts the program by calling {@code Script.intro()}</li>
+ * </ul>
+ */
+public class Main { // 2/4 classes
+
+    /**
+     * The main method launches the game by calling {@link Script#intro()}.
+     *
+     * @param args Command-line arguments (not used).
+     */
+    public static void main(String[] args) {
         Script.intro();
-        
     }
 }
